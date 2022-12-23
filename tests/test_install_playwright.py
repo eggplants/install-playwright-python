9f8e70fa1429fd5c112e09d5766ca44b18678285
eggplants <w10776e8w@yahoo.co.jp>
2022-12-24 04:15:18 +0900

    0.0.0

--- tests/test_install_playwright.py
+++ tests/test_install_playwright.py
@@ -12,16 +12,19 @@ def test_install_sync() -> None:
         res = install(p.chromium)
         assert res is True
 
+
 def test_install_sync_with_deps() -> None:
     with sync_playwright() as p:
         res = install(p.chromium, with_deps=True)
         assert res is True
+
 
 @pytest.mark.asyncio
 async def test_install_async() -> None:
     async with async_playwright() as p:
         res = install(p.chromium)
         assert res is True
+
 
 @pytest.mark.asyncio
 async def test_install_async_with_deps() -> None:
